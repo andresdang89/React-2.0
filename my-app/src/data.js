216597a5b2c39c816be9f04d6978e797d726f7cf
andresdang89 <andresdang89@gmail.com>
2024-02-27 17:38:24 -0500

    primer-commit

---
+++ my-app/src/data.js
@@ -0,0 +1,24 @@
+export const data = [
+	{
+		id: 1,
+		img: './src/Images/anillo-esmeralda-diamantes.avif',
+		nameProduct: 'Anillo de esmeralda con diamantes',
+		price: 2800,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img:'./src/Images/Aretes.diamantes.webp',
+		nameProduct: 'Aretes diamantes y esmeraldas',
+		price: 1500,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: './src/Images/white.webp',
+		nameProduct: 'Pulsera esmeraldas',
+		price: 1000,
+		quantity: 1,
+	},
+	
+];
